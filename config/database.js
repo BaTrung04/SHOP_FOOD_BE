e8ef5aca37204a30068ac180fc80120bb684664c
BaTrung04 <trungmkzxc12345@gmail.com>
2024-10-28 15:59:08 +0700

    be

--- config/database.js
+++ config/database.js
@@ -1,17 +1,13 @@
-const mongoose = require("mongoose");
+const { connect } = require("mongoose");
 
 const connectDatabase = () => {
-  mongoose
-    .connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    })
-    .then((con) => {
-      console.log(
-        `MongoDB Database connected with HOST: ${con.connection.host}`
-      );
-    });
+  connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }).then((con) => {
+    console.log(`MongoDB Database connected with HOST: ${con.connection.host}`);
+  });
 };
 
 module.exports = connectDatabase;
