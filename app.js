25fa93e3188b64a4ffa4e1dd3b5e3408a5bf84ca
BaTrung04 <119580662+BaTrung04@users.noreply.github.com>
2024-12-25 21:52:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://127.0.0.1:5173"
-    ],
+    origin: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
